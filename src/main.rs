35903a68b29588975099d23ab2c456ae2b4a8729
aoikurokawa <aoi.kurokawa@nxted.co.jp>
2024-04-17 17:19:03 +0900

    feat: struct item

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,19 @@ use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    let base_url = "http://127.0.0.1:8001/eth/v1/beacon/states/head/validators";
+    let base_url = "https://qiita.com/api/v2/items?page=1&per_page=20";
     let client = Client::new();
-    let range: Vec<u16> = (0..699).collect();
-    let last = range.last().unwrap();
+    // let range: Vec<u16> = (0..699).collect();
+    // let last = range.last().unwrap();
 
-    let mut pub_keys = String::new();
-    for val in range.clone() {
-        if val == *last {
-            pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b");
-        } else {
-            pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b,");
-        }
-    }
+    // let mut pub_keys = String::new();
+    // for val in range.clone() {
+    //     if val == *last {
+    //         pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b");
+    //     } else {
+    //         pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b,");
+    //     }
+    // }
 
     // let params = [("id", pub_keys)];
 
@@ -27,8 +27,8 @@ async fn main() {
     //     .send()
     //     .await
     //     .unwrap();
-    let url = format!("{}?id={}", base_url, pub_keys);
-    println!("url: {}", url);
+    let url = format!("{}", base_url);
+    // println!("url: {}", url);
 
     let res = reqwest::get(url).await.unwrap();
 
